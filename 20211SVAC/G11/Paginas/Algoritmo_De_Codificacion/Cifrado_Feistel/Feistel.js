318cd103ed72138906518c3d10b5ad6772c387e0
JhonathanTocay2020 <2878571900109@ingenieria.usac.edu.gt>
2021-07-05 19:22:54 -0600

    actualizacion index

--- 20211SVAC/G11/Paginas/Algoritmo_De_Codificacion/Cifrado_Feistel/Feistel.js
+++ 20211SVAC/G11/Paginas/Algoritmo_De_Codificacion/Cifrado_Feistel/Feistel.js
@@ -1,29 +1,162 @@
-let l="01000011",r="01000110";
-let c = "00110001",resultado ="";
+$(document).ready(main);
+//let l="01000011",r="01000110";        0100001101000110
+//let c = "00110001",resultado ="";     00110001
+let izquierda,derecha,clav;
+let l,r,c,resultado;
+var body = document.getElementsByTagName("body")[0];
+var tab = document.getElementById("cla"); 
+var tabla   = document.createElement("table");
+var tblBody = document.createElement("tbody");
 
-console.log("---------- Prueba ----------")
 
-for(ip=0; ip<4;ip++){
-    console.log("----------- Iteracion " + ip + "-----------") 
-    Iteracion();
-    key();
+function main () {    
+    
+	$('.btn-Ingrese').click(function(){
+        var porId = document.getElementById("valor");
+        var cl = document.getElementById("clave");
+        //console.log(porId.value);
+        var paridad =  porId.value.length%2;
+        if (porId.value == ""){
+            alert("ingrese un Dato")
+        }
+        
+        else if(cl.value==""){
+                alert("ingrese una Clave")
+        }
+        else{
+            if (paridad == 0){
+                alert("es par")
+                var tr = porId.value.length/2;
+                var vs = porId.value.length;
+                
+                    
+
+                if(cl.value.length == tr){
+                    l = porId.value.substring(0,tr);
+                    r = porId.value.substring(tr,vs);
+                    c = cl.value;
+                    console.log("iz: " + porId.value.substring(0,tr));
+                    console.log("der: " + porId.value.substring(tr,vs));
+                    //-----------------------------------------------------------------
+                console.log("---------- Prueba ----------")
+
+                for(ip=0; ip<4;ip++){
+                    console.log("----------- Iteracion " + ip + "-----------") 
+                    Iteracion();
+                    key();
+                    tblBody.appendChild(hilera4);    
+                    tabla.appendChild(tblBody);
+                    tab.appendChild(tabla);
+                }
+                console.log(l.concat(r));
+                //--------------------------------------
+                var hileraF = document.createElement("tr");
+                var celdaF1 = document.createElement("td");
+                var celdaF2 = document.createElement("td");
+//--------------------------------------------------------------------------
+                var textoCeldaF1 = document.createTextNode(l);
+                var textoCeldaF2 = document.createTextNode(r);
+                celdaF1.appendChild(textoCeldaF1);
+                hileraF.appendChild(celdaF1);
+                celdaF2.appendChild(textoCeldaF2);
+                hileraF.appendChild(celdaF2);
+//--------------------------------------------------------------------------
+                tblBody.appendChild(hileraF);    
+                tabla.appendChild(tblBody);
+                tab.appendChild(tabla);
+
+  //_-----------------------------------------------
+                    console.log("listo");
+                }
+                else{
+                    alert("Verifique la longitud de la clave")
+                }
+            }else{
+                alert("la cadena ingresada no es par ")
+            }
+        }
+        
+
+        
+        // Limpiar input
+        porId.value ="";
+        porId.focus();
+	});
 }
-console.log(l.concat(r));
 
 function Iteracion(){
     let aux = r;
-
+    var hilera1 = document.createElement("tr");
+    var hilera2 = document.createElement("tr");
+    var hilera3 = document.createElement("tr");
+    var hilera4 = document.createElement("tr");
+    var celda1 = document.createElement("td");
+    var celda2 = document.createElement("td");
+    var celda3 = document.createElement("td");
+    var celda4 = document.createElement("td");
+    var celda5 = document.createElement("td");
+    var celda6 = document.createElement("td");
+    var celda7 = document.createElement("td");
+    var celda8 = document.createElement("td");
+    
+    
+    
+    
     console.log(l.concat(r));
+    var textoCelda1 = document.createTextNode(l);
+    var textoCelda2 = document.createTextNode(r);
+    celda1.appendChild(textoCelda1);
+    hilera1.appendChild(celda1);
+    celda2.appendChild(textoCelda2);
+    hilera1.appendChild(celda2);
+    
+    //------------ Ki ---------------------
     console.log("        "+c);
+    //--------------------------------------------------
+    var textoCelda3 = document.createTextNode("");
+    var textoCelda4 = document.createTextNode(c);
+    celda3.appendChild(textoCelda3);
+    hilera2.appendChild(celda3);
+    celda4.appendChild(textoCelda4);
+    hilera2.appendChild(celda4);
+    
+    //------------------------------------------------
     xm = XOR(r,c);
-    console.log("        "+xm);
+    console.log("        " + xm);
+    //----------------------------------------------------
+    var textoCelda5 = document.createTextNode("");
+    var textoCelda6 = document.createTextNode(xm);
+    celda5.appendChild(textoCelda5);
+    hilera3.appendChild(celda5);
+    celda6.appendChild(textoCelda6);
+    hilera3.appendChild(celda6);
+    //-------------------------------------------------------
     console.log("        "+l);
+    var textoCelda7 = document.createTextNode("");
+    var textoCelda8 = document.createTextNode(l);
+    celda7.appendChild(textoCelda7);
+    hilera4.appendChild(celda7);
+    celda8.appendChild(textoCelda8);
+    hilera4.appendChild(celda8);
     xj = XOR(xm,l);
     //console.log("        "+xj);
     l= r;
     r = xj;
     //console.log(l.concat(r));
     //console.log("--------------------------------------")
+
+    
+    tblBody.appendChild(hilera1);
+    tblBody.appendChild(hilera2);
+    tblBody.appendChild(hilera3);
+    tblBody.appendChild(hilera4);
+    
+    tabla.appendChild(tblBody);
+    // appends <table> into <body>
+    tab.appendChild(tabla);
+    // modifica el atributo "border" de la tabla y lo fija a "2";
+    tabla.setAttribute("border", "2");
+    
 }
 
 
@@ -53,5 +186,4 @@ function key(){
         }
     }
     c = auxX.concat(auxJ);
-    //console.log(c);
 }
