0ebdd23167ef6425838e949c80d57b73e7688e30
JhonathanTocay2020 <2878571900109@ingenieria.usac.edu.gt>
2021-07-07 19:14:21 -0600

    ultimo

--- 20211SVAC/G11/Paginas/Algoritmo_De_Codificacion/Algoritmo_LZW/LZW.js
+++ 20211SVAC/G11/Paginas/Algoritmo_De_Codificacion/Algoritmo_LZW/LZW.js
@@ -1,22 +1,40 @@
-//let mensaje = "PABLO PAPA DE PABLITO";
-let mensaje = "sobornos_son_robos";
-//let mensaje = "ABACABA";
-//let mensaje = "aabababaaa";
+$(document).ready(main);
+//let mensaje = "sobornos_son_robos";
 let diccionario = [];
 let auxDiccionario = [];
 let recientes = [];
 let iteraciones =[];
 
-console.log("Original: " + mensaje);
-cambiar_espacios();
-console.log("Sustitucion: " + mensaje);
-verificar_Existencia();
-console.log(diccionario);
-console.log("Corre Diccionario");
-algoritmo_LZW();
-console.log(diccionario);
-//console.log("----------------------------------------");
-console.log(iteraciones);
+function main () {    
+    $('.btn-Guardar').click(function(){
+        p_datos()
+      });
+	$('.btn-Ingrese').click(function(){
+        diccionario=[];
+        auxDiccionario=[];
+        recientes =[];
+        iteraciones=[];
+
+        var porId = document.getElementById("valor");
+        mensaje = porId.value;
+        console.log("Original: " + mensaje);
+        cambiar_espacios();
+        console.log("Sustitucion: " + mensaje);
+        verificar_Existencia();
+        console.log(diccionario);
+        console.log("Corre Diccionario");
+        algoritmo_LZW();
+        console.log(diccionario);
+    //console.log("----------------------------------------");
+        console.log(iteraciones);
+        imprimir();
+    });
+}
+//let mensaje = "PABLO PAPA DE PABLITO";
+
+//let mensaje = "ABACABA";
+//let mensaje = "aabababaaa";
+
 // -------------- Funciones -------------- 
 function cambiar_espacios(){
     aux = "";
@@ -93,4 +111,19 @@ function it(w,k,wk,d,s){
     //    s = ""
         iteraciones.push({"w": w, "k": k,"wk":wk,"dic": d,"salida":s});
     //}    
+}
+
+function imprimir(){
+    //var hmtl = document.getElementById("cla");
+    document.getElementById("cla").innerHTML = "<h2>Iteraciones</h2><br>";
+    
+    for(msn in iteraciones){
+        document.getElementById("cla").innerHTML += '<p>iteracion: '+ msn +'</p>';
+        document.getElementById("cla").innerHTML += '<p>w: '+iteraciones[msn].w+'</p>';
+        document.getElementById("cla").innerHTML += '<p>k: '+iteraciones[msn].k+''+'</p>';        
+        document.getElementById("cla").innerHTML += '<p>wk: '+iteraciones[msn].k+''+'</p>';        
+        document.getElementById("cla").innerHTML += '<p>dicionario: '+iteraciones[msn].k+''+'</p>';
+        document.getElementById("cla").innerHTML += '<p>salida: '+iteraciones[msn].k+''+'</p>';
+    }
+    
 }
